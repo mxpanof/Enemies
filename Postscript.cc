5b4c4091ca8cf7cf75115c28aed685fd3bdf9cb0
Laura <laurartesoriero@gmail.com>
2018-03-28 21:07:39 -0400

    .cc file

---
+++ Postscript.cc
@@ -0,0 +1,48 @@
+#include <Postscript.h>
+#include <iostream>
+#include <fstream>
+#include <string>
+
+Class Postscript{
+	public:
+	Postscript (string newFileName) {
+		ostream writer(newFileName);
+		filename = newFileName;
+	}
+	~Postscript (){
+		writer.close();
+	};  
+	
+	void drawRect(int startx,  int starty,  int addx,  int addy) const{
+		
+	}
+	
+	void fillRect ( int startx,  int starty,  int addx,  int addy) const {
+		
+	}
+	void setFillColor (  int R,  int G,  int B) const {
+		
+	}
+	void setStrokeColor(int r, int g, int b) const {
+		
+	}
+	
+	void drawTriangle(int trix1,int triy1, int trix2, int triy2, int trix3, int triy3) const{
+		
+	}
+	void fillTriangle(int trix1,int triy1,int trix2, int triy2,int trix3, int triy3) const{
+		
+	}
+	void drawCircle(int trix,int triy,int trir) const {
+		
+	}
+	void text(int trix, int triy,string phrase)const{
+		
+	}
+	void grid(int arg1, int arg2, int arg3, int arg4, int arg4, int arg5, int arg6) const{
+		
+	}
+	
+	private:
+	
+};
