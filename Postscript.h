f272fa394f6ef8ef21cc3cf0e5f008f3b55f7a0e
Laura <laurartesoriero@gmail.com>
2018-03-28 20:28:00 -0400

    added const to .h

--- Postscript.h
+++ Postscript.h
@@ -6,14 +6,14 @@ using namespace std;
 class Postscript
 {
 public:
-void line (int startx, int starty, int endx, int endy);
-void setColor (int R, int g, int G);
+void line (int startx, int starty, int endx, int endy)const;
+void setColor (int R, int g, int G)const;
 
-void setStrokeColor(int r, int g, int b);
-void drawTriangle(x1,y1, x2,y2, x3,y3);
-void fillTriangle(x1,y1, x2,y2, x3,y3);
-void drawCircle(x,y,r); 
-void text(x,y, "testing testing 123");
+void setStrokeColor(int r, int g, int b) const;
+void drawTriangle(x1,y1, x2,y2, x3,y3) const;
+void fillTriangle(x1,y1, x2,y2, x3,y3) const;
+void drawCircle(x,y,r) const; 
+void text(x,y, "testing testing 123")const;
 
 
 };
