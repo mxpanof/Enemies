404cee46b3312a09b82c854b37d45ff15ecc9787
Laura <laurartesoriero@gmail.com>
2018-03-28 20:44:04 -0400

    changes to the .h file

--- Postscript.h
+++ Postscript.h
@@ -8,22 +8,14 @@ using namespace std;
 class Postscript
 {
 public:
-<<<<<<< HEAD
-void line ( int startx,  int starty,  int endx,  int endy) const ;
-void setColor ( int R,  int G, int B) ;
+
 void drawrect ( int startx,  int starty,  int addx,  int addy) const ;
 void fillRect ( int startx,  int starty,  int addx,  int addy) const ;
 void setFillColor (  int R,  int G,  int B) const ;
-=======
-void line (int startx, int starty, int endx, int endy)const;
-void setColor (int R, int g, int G)const;
-
 void setStrokeColor(int r, int g, int b) const;
-void drawTriangle(x1,y1, x2,y2, x3,y3) const;
-void fillTriangle(x1,y1, x2,y2, x3,y3) const;
-void drawCircle(x,y,r) const; 
-void text(x,y, "testing testing 123")const;
-
->>>>>>> f272fa394f6ef8ef21cc3cf0e5f008f3b55f7a0e
+void drawTriangle(int trix1,int triy1, int trix2, int triy2, int trix3, int triy3) const;
+void fillTriangle(int trix1,int triy1,int trix2, int triy2,int trix3, int triy3) const;
+void drawCircle(int trix,int triy,int trir) const; 
+void text(int trix, int triy,string phrase)const;
 
 };
