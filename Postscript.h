6bb1dfd5e1d67f1494f8e778fadf52df9a3bc634
Laura <laurartesoriero@gmail.com>
2018-03-28 20:26:12 -0400

    adding to the .h file

--- Postscript.h
+++ Postscript.h
@@ -9,4 +9,11 @@ public:
 void line (int startx, int starty, int endx, int endy);
 void setColor (int R, int g, int G);
 
+void setStrokeColor(int r, int g, int b);
+void drawTriangle(x1,y1, x2,y2, x3,y3);
+void fillTriangle(x1,y1, x2,y2, x3,y3);
+void drawCircle(x,y,r); 
+void text(x,y, "testing testing 123");
+
+
 };
