4fcdf9eb6ad2e3ac39d1980dfe2e73b674147a18
Laura <laurartesoriero@gmail.com>
2018-04-04 18:07:03 -0400

    updated code

--- Postscript.cc
+++ Postscript.cc
@@ -19,20 +19,20 @@ Class Postscript{
 	}
 	
 	void fillRect ( int startx,  int starty,  int addx,  int addy) const {
-		writer.append() << startx << " " << starty << " moveto " << addx+startx << " " << addy << " " << " lineto " << addx+startx << " " << addy+starty << " lineto " << startx << " " << endy+starty << " closepath fill\n";
+		writer.append() << startx << " " << starty << " moveto " << addx+startx << " " << starty << " " << " lineto " << addx+startx << " " << addy+starty << " lineto " << startx << " " << endy+starty << " closepath fill\n";
 	}
 	void setFillColor (  int R,  int G,  int B) const {
-		
+		writer.append() << R << " " << G << " " << B << " setrgbcolor fill /n";    
 	}
 	void setStrokeColor(int r, int g, int b) const {
-		
+		writer.append() << R << " " << G << " " << B << " setrgbcolor stroke /n";
 	}
 	
 	void drawTriangle(int trix1,int triy1, int trix2, int triy2, int trix3, int triy3) const{
-		
+		writer.append() << trix1 << " " << triy1 << " moveto " << trix2 << " " << triy2 << " lineto " << trix3 << " " << triy3 << " closepath stroke\n " ;
 	}
 	void fillTriangle(int trix1,int triy1,int trix2, int triy2,int trix3, int triy3) const{
-		
+		writer.append() << trix1 << " " << triy1 << " moveto " << trix2 << " " << triy2 << " lineto " << trix3 << " " << triy3 << " closepath fill\n " ;
 	}
 	void drawCircle(int trix,int triy,int trir) const {
 		
